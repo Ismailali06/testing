5b6eea4dce024bceb160af421af7a9cb8c82e3af
Ismailali06 <65562577+Ismailali06@users.noreply.github.com>
2020-07-20 17:53:43 -0400

    Updated chrome driver

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,8 @@
-print ('hello World')
-print ('seccond time hello world')
+from selenium import webdriver
+
+
+driver = webdriver.Chrome()
+driver.get('https://google.com/')
+driver.quit()
+print ("sucess")
+
